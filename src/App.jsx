9d660c13fae3785ef68327c2f22401a704fdbbee
Franklim Arboleda Falla <franklim.arboleda@gmail.com>
2024-04-11 23:08:48 -0500

    Creacion de Pages

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-import './App.css';
-
-function App() {
-   return <div className=' bg-green-300'>Hola mundo!!!</div>;
-}
-
-export default App;
---
+++ src/Pages/MyAccount/index.jsx
@@ -0,0 +1,5 @@
+function MyAccount() {
+   return <div className=' bg-green-300'>MyAccount</div>;
+}
+
+export default MyAccount;
