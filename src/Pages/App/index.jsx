a8068be9a274b298e5872541031576c2c45f94c6
Franklim Arboleda Falla <franklim.arboleda@gmail.com>
2024-04-12 20:09:53 -0500

    Se agrega el enrutamiento

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,24 +1,55 @@
+//Rutas
+import { useRoutes, BrowserRouter } from 'react-router-dom';
+
 //Páginas
 import Home from '../Home';
-import MyAccount from '../Home';
+import MyAccount from '../MyAccount';
 import MyOrders from '../MyOrders';
 import MyOrder from '../MyOrder';
 import NotFound from '../NotFound';
 import SignIn from '../SignIn';
 
+//Estilos
 import './App.css';
 
-function App() {
+//Función con las rutas
+const AppRoutes = () => {
+   let routes = useRoutes([
+      {
+         path: '/',
+         element: <Home />,
+      },
+      {
+         path: '/my-account',
+         element: <MyAccount />,
+      },
+      {
+         path: '/my-orders',
+         element: <MyOrders />,
+      },
+      {
+         path: '/my-order',
+         element: <MyOrder />,
+      },
+      {
+         path: '/sign-in',
+         element: <SignIn />,
+      },
+      {
+         path: '/*',
+         element: <NotFound />,
+      },
+   ]);
+
+   return routes;
+};
+
+const App = () => {
    return (
-      <div className=' bg-green-300'>
-         <Home />
-         <MyAccount />
-         <MyOrders />
-         <MyOrder />
-         <NotFound />
-         <SignIn />
-      </div>
+      <BrowserRouter>
+         <AppRoutes />
+      </BrowserRouter>
    );
-}
+};
 
 export default App;
