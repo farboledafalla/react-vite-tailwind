81d09178debd68804d20e7fbf1a7edfef44f82ff
Franklim Arboleda Falla <franklim.arboleda@gmail.com>
2024-04-20 13:50:18 -0500

    Se organiza el flujo para ver la orden despues de hacer checkout

---
+++ src/Components/OrdersCard/index.jsx
@@ -0,0 +1,24 @@
+// PropTypes
+import PropTypes from 'prop-types';
+
+// Iconos
+// import { XMarkIcon } from '@heroicons/react/24/solid';
+
+export const OrdersCard = (props) => {
+   const { totalPrice, totalProducts } = props;
+
+   return (
+      <div className='flex justify-between items-center mb-3 border border-black'>
+         <p>
+            <span>01.02.2023</span>
+            <span>{totalProducts}</span>
+            <span>{totalPrice}</span>
+         </p>
+      </div>
+   );
+};
+
+OrdersCard.propTypes = {
+   totalPrice: PropTypes.number,
+   totalProducts: PropTypes.number,
+};
