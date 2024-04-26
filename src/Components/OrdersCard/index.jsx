abc91fdc827e5e78e64ad72f2c37d52d81a6f4f9
Franklim Arboleda Falla <franklim.arboleda@gmail.com>
2024-04-25 20:02:01 -0500

    Se agregan estilos a la lista de ordenes

--- src/Components/OrdersCard/index.jsx
+++ src/Components/OrdersCard/index.jsx
@@ -2,18 +2,23 @@
 import PropTypes from 'prop-types';
 
 // Iconos
-// import { XMarkIcon } from '@heroicons/react/24/solid';
+import { ChevronRightIcon } from '@heroicons/react/24/solid';
 
 export const OrdersCard = (props) => {
    const { totalPrice, totalProducts } = props;
 
    return (
-      <div className='flex justify-between items-center mb-3 border border-black'>
-         <p>
-            <span>01.02.2023</span>
-            <span>{totalProducts}</span>
-            <span>{totalPrice}</span>
-         </p>
+      <div className='flex justify-between items-center mb-4 border border-black w-80 p-4 rounded-lg'>
+         <div className='flex justify-between w-full'>
+            <p className='flex flex-col'>
+               <span className='font-light'>01.02.2023</span>
+               <span className='font-light'>{totalProducts} articles</span>
+            </p>
+            <p className='flex gap-2 items-center'>
+               <span className='font-medium text-2xl'>${totalPrice}</span>
+               <ChevronRightIcon className='h-5 w-5 text-black cursor-pointer' />
+            </p>
+         </div>
       </div>
    );
 };
--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -10,8 +10,8 @@ function MyOrders() {
 
    return (
       <Layout>
-         <div className='flex w-80 items-center relative justify-center'>
-            <h1>My Orders</h1>
+         <div className='flex w-80 items-center relative justify-center mb-4'>
+            <h1 className='font-medium text-xl'>My Orders</h1>
          </div>
          {context.order.map((order, index) => (
             <Link key={index} to={`/my-orders/${index}`}>
